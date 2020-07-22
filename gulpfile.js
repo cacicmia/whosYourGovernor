cbb37f6c2352e3a4d0a7a07b0f9ffe81c8eb6ac2
Mia Ćaćić <cacic.miaa@gmail.com>
2020-07-22 09:32:24 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const { src, dest, parallel, series, watch} = require('gulp')
+const sass = require('gulp-sass')
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps')
+// var cssnano = require('gulp-cssnano');
+const webpack = require('webpack')
+const webpackStream = require('webpack-stream')
+const webpackConfig = require('./webpack.config.js')
+const browserSync = require('browser-sync').create()
+var autoprefixer = require('autoprefixer')
+function html(done) {
+    src('src/*.html')
+    .pipe(dest('dist'))
+    .pipe(browserSync.stream())
+    done()
+}
+function styles(done) {
+    src('src/scss/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([autoprefixer()]))    
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('dist/css'))
+    
+    .pipe(browserSync.stream())
+    done();
+}
+function scripts(done) {
+    src('src/js/**')
+        .pipe(webpackStream(webpackConfig), webpack)
+        .pipe(dest('dist/js'))
+        .pipe(browserSync.stream())
+        done()   
+}
+function serve(done) {
+    browserSync.init({
+        server: 'dist'
+    }, done)
+}
+function reload(done) {
+    browserSync.reload()
+    done()
+}
+function watchTask(done) {
+    
+    watch('src/js/**', series(scripts))
+    watch('src/scss/**', series(styles))
+    watch('src/index.html', series(html))
+    done()
+}
+
+
+exports.watch = watchTask
+exports.scripts = scripts
+exports.styles = styles
+exports.serve = serve
+exports.build = parallel(styles, scripts, )
+exports.default = series(serve, watchTask)
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const path = require('path')
+
+module.exports = {
+    entry: path.resolve(__dirname, './src/js/index.js'),
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm.js'
+        }
+    },
+    output: {
+        filename: 'index.js',
+    //     path: path.resolve(__dirname, './dist/js/'),
+
+    },
+    // context: path.resolve(__dirname, '../d'),
+    module: {
+        rules: [
+            // ... other rules
+            {
+                test: /\.vue$/,
+                exclude: /(node_modules)/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader'
+            },
+           
+          ]
+    },
+    plugins: [
+        // make sure to include the plugin!
+        new VueLoaderPlugin()
+    ]
+}
