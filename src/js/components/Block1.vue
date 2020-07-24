98d693cbe73b97ea34f5887fcb46a82d8ba5bbcf
Mia Ćaćić <cacic.miaa@gmail.com>
2020-07-24 22:20:47 +0200

    details, responsive

--- src/js/components/Block1.vue
+++ src/js/components/Block1.vue
@@ -4,20 +4,20 @@
             <h1>{{$t("block1.heading1")}} <b>{{$t("block1.heading2")}}</b></h1>
         </div>
         <div class="block1__flex-container">
-            <div class="block1__form-container">
+            <form class="block1__form-container">
                 <div class="block1__input-fields-container">
                     <label for="county">{{$t('block1.county_label')}}</label>
                     
-                    <Select2 ref="countyValue" v-model="countyValue" :settings="countySelectSettings" :options="countyOptions" @change="changeCounty" id="county" name="county" />
+                    <Select2 ref="countyValue" v-model="countyValue" :settings="countySelectSettings" :options="countyOptions" @change="changeCounty" id="county" name="county" :required="true" />
                     <label for="town">{{$t('block1.town_label')}}</label>
-                    <Select2 ref="townValue" v-model="townValue" :settings="townSelectSettings" :options="townOptions" @select="townUpdate" id="town" name="town" />
+                    <Select2 ref="townValue" v-model="townValue" :settings="townSelectSettings" :options="townOptions" @select="townUpdate" id="town" name="town" :required="true"/>
                 </div>
-                <div class="form__button-container">
-                    <button class="btn btn-primary">
+                <div class="block1__button-container">
+                    <button class="btn btn-primary" @click="submitData">
                         {{$t('block1.submit_button_text')}}
                     </button>
                 </div>
-            </div>
+            </form>
             <div class="block1__svg-container">
                 <div class="block1__svg-background"></div>
             </div>
@@ -29,9 +29,11 @@
 import $ from "jquery";
 import Select2 from 'v-select2-component';
 import axios from 'axios'
+import EventBus from '../event-bus';
 export default {
     components: {
-        Select2
+        Select2, 
+        EventBus
     },
     props: {
         initialListURL: {
@@ -50,7 +52,10 @@ export default {
             detailsUrl: this.initialDetailsUrl,
             // la.search = 'v=1&z=2'
             countyOptions: [],
-            townOptions: []
+            townOptions: [],
+            entityType: '',
+            entityID: '',
+            detailsData: {}
             
         }
     }, 
@@ -59,7 +64,6 @@ export default {
             let vm = this
             axios.get(this.initialListURL)
             .then(response => {
-                console.log(response)
                 let countyResults =  response.data.counties.map((county)=> ({
                         id: county.ID,
                         text: county.name  
@@ -72,7 +76,9 @@ export default {
                 townResults = townResults.map((entity)=> ({
                         id: `${entity.ID}-${entity.name}`,
                         text: entity.name,
-                        countyID: entity.countyID
+                        entityID: entity.ID,
+                        countyID: entity.countyID,
+                        entityType: entity.entityType
 
                         }
                     ))
@@ -82,7 +88,6 @@ export default {
         },
 
         changeCounty(value) {
-            console.log(value)
             if (!value.length){
                 this.townValue = []
             }
@@ -93,7 +98,33 @@ export default {
             if (!this.countyValue.length){
                 this.countyValue = [newValue.countyID]
             }
-        },   
+            if (newValue.entityID){
+
+                this.entityType = newValue.entityType;
+                this.entityID = newValue.entityID;
+            } else {
+                this.entityType = '';
+                this.entityID = '';
+            }
+           
+        },  
+        submitData(e) {
+            e.preventDefault()
+            const vm = this
+            if (this.entityType && this.entityID){
+                let url = this.initialDetailsUrl;
+                url =url.concat(`?entityType=${this.entityType}&ID=${this.entityID}`)
+                axios.get(url)
+                .then(response => {
+                    vm.detailsData = response.data
+                    vm.emitData()
+                })
+                .catch(error=> console.error(error))
+            }
+        },
+        emitData() {
+            EventBus.$emit('details_update', this.detailsData);
+        } 
     },
     mounted() {
         this.initOptions()
--- src/js/components/Block2.vue
+++ src/js/components/Block2.vue
@@ -3,15 +3,59 @@
         <div class="block2__heading-container">
             <h2>{{$t("block2.heading")}} </h2>
         </div>
-        <div class="block2__info-wrappper content-container content-container--padded">
+        <div class="block2__info-wrappper content-container content-container--padded content-container--right" v-if="details.ID">
+            <div class="block2__column" >
+                <div class="block2__row" v-for="key in keys1" :key="key">
+                    <p>{{$t(`block2.${key}`)}}</p>
+                    <p>{{details[key] ? details[key] : $t(`block2.no_info.${key}`)}}</p>
+                </div>
+                
+            </div>
+            <div class="block2__column">
+                <div class="block2__row" v-for="key in keys2" :key="key">
+                    <p>{{$t(`block2.${key}`)}}</p>
+                    <p v-if="key=='phone'"> <a :href="`tel:${details[key]}`">{{details[key] ? details[key] : $t(`block2.no_info.${key}`)}} </a></p>
+                    <p v-else-if="key=='email'"><a :href="`mailto:${details[key]}`">{{details[key] ? details[key] : $t(`block2.no_info.${key}`)}} </a></p>
+                    <p v-else-if="key=='web'"><a :href="`${details[key]}`">{{details[key] ? details[key] : $t(`block2.no_info.${key}`)}} </a></p>
+                    <p v-else >{{details[key] ? details[key] : $t(`block2.no_info.${key}`)}}</p>
+                </div>
+            </div>
 
         </div>
     </div>
 </template>
 
 <script>
+import EventBus from '../event-bus';
 export default {
+    props: {
+        keys1:{
+            type: Array,
+            default: ()=> ['governor', 'viceGovernor', 'address', 'zipCode', 'country']
+        },
+        keys2: {
+            type: Array,
+            default: ()=> ['web', 'email', 'phone', 'fax'] 
+        }
+    },
+    mounted () {
+        EventBus.$on('details_update', this.updateData)
+    },
+    data() {
+        return {
+            details: {}
+        }
+    },
+    methods: {
+        updateData(details) {
+            console.log(details)
+            this.details = details
 
+        }
+    },
+    computed: {
+       
+    }
 }
 </script>
 
